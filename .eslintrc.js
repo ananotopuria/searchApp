42e7b1c47a901ab1e9deca5e749bb80fb0214fdb
Anano Topuria <anano.topuriaa@gmail.com>
2025-01-31 14:13:57 +0400

    Updates

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    extends: ["plugin:react/recommended", "plugin:@typescript-eslint/recommended", "prettier"],
+    rules: {
+      "@typescript-eslint/no-explicit-any": "error",
+      "react/no-unescaped-entities": "off"
+    }
+  };
+  
