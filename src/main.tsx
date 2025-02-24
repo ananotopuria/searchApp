800c10951b1f0af2ca0d65f460c88df2656ab288
Anano Topuria <anano.topuriaa@gmail.com>
2025-02-24 19:29:52 +0400

    fix: wrap App with BrowserRouter and set basename for GitHub Pages

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,17 @@ import App from './App';
 import { Provider } from 'react-redux';
 import { store } from './store';
 import { ThemeProvider } from './context/ThemeProvider';
-import "./index.css"
-import "./App.css"
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
+import './App.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
       <ThemeProvider>
-        <App />
+        <BrowserRouter basename="/searchApp">
+          <App />
+        </BrowserRouter>
       </ThemeProvider>
     </Provider>
   </React.StrictMode>,
